0e43fc85e28cc82b13b845b37870a4cb29c51764
Braanah Craig <braanah.craig@gmail.com>
2018-11-30 16:21:25 -0500

    updated sqlite database to include voting. Wrote function to export csv that may or may not be functioning

--- user.rb
+++ user.rb
@@ -10,7 +10,10 @@ class User
   property :userName, String
   property :role, String
   property :password, String
-  property :vote, String
+  property :vote, Boolean
+  property :firstPlace, String
+  property :secondPlace, String
+  property :thirdPlace, String
 end
 
 DataMapper.finalize()
@@ -18,7 +21,15 @@ DataMapper.finalize()
 def addUsersFromCSV(filepath)
   csvUsers = CSV.read(filepath)
   for users in csvUsers
-    User.create(userName: users[0], role: users[2], password: users[1], vote: "No")
+    User.create(userName: users[0], role: users[2], password: users[1], vote: FALSE)
+  end
+end
+
+def writeVotesToCsv(voters)
+  CSV.open("voterData.csv", "wb") do |csv|
+    for voter in voters
+      csv << [voter.id, voter.userName, voter.firstPlace, voter.secondPlace, voter.thirdPlace]
+    end
   end
 end
 
